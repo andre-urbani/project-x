77be3d4c125049182a714867b69c40cad030a612
dredizzle <dredizzle@live.co.uk>
2019-12-12 21:23:20 +0000

    fixed postcode API fetch on submit

--- project-x/src/components/Home.js
+++ project-x/src/components/Home.js
@@ -1,23 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { Link } from 'react-router-dom'
 
-const Home = () => {
+const Home = (props) => {
+
+  const [postCode, setPostcode] = useState({
+
+    pCode: ''
+
+  })
+
+  // const [geo, setGeo] = useState([])
+
+  const fetchRequest = useCallback((e) => {
+    e.preventDefault()
+    fetch(`https://api.postcodes.io/postcodes/${postCode.pCode}`)
+      
+      .then(res => res.json())
+      .then(res => {
+        const test = res
+        props.history.push('/maps', test)
+        // setGeo(res)
+      })
 
 
 
+  }, [{ ...postCode }])
 
+
+  const handleChange = (e) => {
+    setPostcode({ ...postCode, pCode: e.target.value })
+   
   
+  }
+
+
+
   return <section >
-    <div className='container'>
-      {/* <form className="form" onSubmit={handleSubmit}>
-        <input className="input" type="text" placeholder="Text input"/>
-
-      </form> */}
-      <Link className='button' to={{
-        pathname: '/maps'
-      }}>Map
   
-      </Link>
+    <div className='container'>
+
+      <form className="form" onSubmit={fetchRequest}>
+        <input className="input" type="text" placeholder="Text input" onChange={handleChange} />
+        < button>submit</button>
+        <Link className='button' to={{
+          pathname: '/maps'
+        }}>Map
+    
+        </Link>
+      </form>
+
     </div>
 
   </section>
--- project-x/src/components/Maps.js
+++ project-x/src/components/Maps.js
@@ -2,35 +2,36 @@ import React, { useState, useEffect, useCallback } from 'react'
 import ReactMapGL, { GeolocateControl, Marker, Popup } from 'react-map-gl'
 
 const TOKEN = 'pk.eyJ1IjoiZHJlZGl6emxlIiwiYSI6ImNrM2o2d3Q2ODBlbGMzanFsM3RtMG8waGsifQ.y8DoH4fhWbGPzd_VkDHIQg'
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMWZmNTRlZjEtYWIxZC00MWQzLWE1ZTAtZDE1ZDU1MTE3OWRjIiwia2V5X2lkIjoiNDQ0OTlkZmYtMTFjZS00N2QxLTk0MDEtMWMyNzVkN2RhZDRhIiwiaWF0IjoxNTc1NDU4ODk5fQ.kyAjrIZMZWjk7j6ZSpXp-I4XZrWLKiCgKpUrnGZ986g'
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiODU0NzlhNzItZmZmZi00MWIwLWFkOGEtOWY2N2ViY2Y3MGI4Iiwia2V5X2lkIjoiYWNkY2I2NzAtY2Y5My00M2FmLThhZWYtMzUwNjU3MTIwYjMyIiwiaWF0IjoxNTc2MTg0NTgzfQ.lnIfptwPKNwU72QmkIsKsdr2Aen08hqATcjfdkoi3tU'
 
-const Maps = () => {
+const Maps = (props) => {
 
   const [events, setEvents] = useState([])
 
   const fetchRequest = useCallback(() => {
 
-    fetch(`https://api.list.co.uk/v1/events?near=${viewport.latitude},${viewport.longitude}/10`, {
+    fetch(`https://api.list.co.uk/v1/events?near=${viewport.latitude},${viewport.longitude}/5`, {
       headers: {
         'Authorization': `Bearer ${API_KEY}`
       }
     })
       .then(res => res.json())
       .then(res => setEvents(res))
-    console.log(events)
 
 
   }, [{ ...viewport }])
 
   useEffect(() => {
+    
 
-    fetch(`https://api.list.co.uk/v1/events?near=${viewport.latitude},${viewport.longitude}/10`, {
+    fetch(`https://api.list.co.uk/v1/events?near=${props.location.state.result.latitude},${props.location.state.result.longitude}/5`, {
       headers: {
         'Authorization': `Bearer ${API_KEY}`
       }
     })
       .then(res => res.json())
       .then(res => setEvents(res))
+      .then(console.log(props.location))
 
     return () => console.log('Unmounting component')
 
@@ -39,9 +40,9 @@ const Maps = () => {
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
-    latitude: 51.45523,
-    longitude: -2.59665,
-    zoom: 13.5
+    latitude: props.location.state.result.latitude,
+    longitude: props.location.state.result.longitude,
+    zoom: 12
 
   })
 
